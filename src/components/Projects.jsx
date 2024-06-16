aad47753cddf23fd287ce70cd78279cfa00ba794
MarkMallari16 <mallariftw01@gmail.com>
2024-06-16 23:28:25 +0800

    fix some layout in projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa";
 
 import OnlineVotingPicture from '../assets/onlinevoting-ss.png'
 import DesktopTodoPicture from '../assets/todo-ss1.png'
-import ProjectDetails from './ProjectDetails';
+import SingleProject from './SingeProject';
 
 function Projects() {
     return (
@@ -13,7 +13,7 @@ function Projects() {
             <div className='text-medium text-center text-3xl font-medium pt-10'>My Projects</div>
             <p className='text-center text-slate-400 font-light'>Here are my recent works</p>
             <div className='px-4 lg:px-0 grid grid-cols-1 lg:grid-cols-2 gap-14 py-20'>
-                <div>
+                <div className='pt-10'>
                     <div className='w-full h-72 bg-base-300 rounded-xl' style={{
                         backgroundImage: `url('${OnlineVotingPicture}')`,
                         backgroundSize: 'cover'
@@ -39,42 +39,13 @@ function Projects() {
                     </div>
                 </div>
 
-                <div>
-                    <div className='w-full h-80 bg-base-300 rounded-xl' style={{
-                        backgroundImage: `url('${DesktopTodoPicture}')`,
-                        backgroundSize: 'cover'
-                    }}></div>
-                    <div className='w-full h-60  rounded-xl'>
-                        <h1 className='pt-8 text-2xl font-medium'>To-do List Application</h1>
-                        <p className='mt-4 text-lg'>I developed a user-friendly To-do list application to help users manage their daily tasks efficiently. Built with React JS and styled with Tailwind CSS and daisy UI, it features an intuitive interface for adding, editing, and organizing tasks.</p>
-
-                        <div className='flex mt-3 gap-3'>
-                            <SkillIcon languageName='React JS' />
-                            <SkillIcon languageName='Tailwind' />
-
-                        </div>
-
-                        <div className='mt-6 flex gap-3'>
-                            <a href="https://simple-todo-list-mark.netlify.app/" target='_blank' className='btn btn-outline'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                </svg>
-
-                                View Live</a>
-                            <a href="" className='btn btn-outline'>
-                                <FaGithub className='text-xl' />
-                                View Repository</a>
-                        </div>
-                    </div>
-
-
-                </div>
-
-                <ProjectDetails projectPicture={DesktopTodoPicture} projectTitle="To-do List Application" projectDescripton="I developed a user-friendly To-do list application to help users manage their daily tasks efficiently. Built with React JS and styled with Tailwind CSS and daisy UI, it features an intuitive interface for adding, editing, and organizing tasks."
+                <SingleProject projectPicture={OnlineVotingPicture} projectTitle="Online Voting System at STI College Bacoor SHS student council" projectDescripton="This web application allows students to vote easily. It is my first capstone project, and it won Best in User Interface and System Development at the SHS Expo 2024 at our school."
                     projectGithubRepository="" />
 
-                <ProjectDetails projectPicture={DesktopTodoPicture} projectTitle="To-do List Application" projectDescripton="I developed a user-friendly To-do list application to help users manage their daily tasks efficiently. Built with React JS and styled with Tailwind CSS and daisy UI, it features an intuitive interface for adding, editing, and organizing tasks."
+                <SingleProject projectPicture={DesktopTodoPicture} projectTitle="To-do List Application" projectDescripton="I developed a user-friendly To-do list application to help users manage their daily tasks efficiently. Built with React JS and styled with Tailwind CSS and daisy UI, it features an intuitive interface for adding, editing, and organizing tasks."
+                    projectLiveURL="https://simple-todo-list-mark.netlify.app/" projectGithubRepository="" />
+
+                <SingleProject projectPicture={DesktopTodoPicture} projectTitle="To-do List Application" projectDescripton="I developed a user-friendly To-do list application to help users manage their daily tasks efficiently. Built with React JS and styled with Tailwind CSS and daisy UI, it features an intuitive interface for adding, editing, and organizing tasks."
                     projectGithubRepository="" />
             </div>
         </div>
--- src/components/SingeProject.jsx
+++ src/components/SingeProject.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { FaGithub } from 'react-icons/fa'
 import SkillIcon from './SkillIcon'
 
-function ProjectDetails({ projectPicture, projectTitle, projectDescripton, projectGithubRepository }) {
+function SingeProject({ projectPicture, projectTitle, projectDescripton, projectLiveURL = null, projectGithubRepository }) {
     return (
 
-        <div className='py-20'>
+        <div className='py-10'>
             <div className='w-full h-80 bg-base-300 rounded-xl' style={{
                 backgroundImage: `url('${projectPicture}')`,
                 backgroundSize: 'cover'
@@ -21,7 +21,15 @@ function ProjectDetails({ projectPicture, projectTitle, projectDescripton, proje
                 </div>
 
                 <div className='mt-6 flex gap-3'>
+                    {projectLiveURL && (
+                        <a href={projectLiveURL} target='_blank' className='btn btn-outline'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
 
+                            View Live</a>
+                    )}
                     <a href={projectGithubRepository} className='btn btn-outline'>
                         <FaGithub className='text-xl' />
                         View Repository</a>
@@ -31,4 +39,4 @@ function ProjectDetails({ projectPicture, projectTitle, projectDescripton, proje
     )
 }
 
-export default ProjectDetails
+export default SingeProject
