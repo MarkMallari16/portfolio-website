39cebd58fb70c61d8081f0e4f5659759dbe09115
MarkMallari16 <mallariftw01@gmail.com>
2024-06-27 20:09:05 +0800

    fix some ui

--- src/components/SkillIcon.jsx
+++ src/components/SkillIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SkillIcon({ icon, languageName }) {
     return (
-        <div className='flex border px-4 py-1 rounded-xl border-gra bg-base-200 hover:bg-secondary hover:border-secondary hover:text-white transition-all ease-in-out duration-150'>
+        <div className='flex border px-4 py-1 rounded-lg border-gra bg-base-200 hover:bg-secondary hover:border-secondary hover:text-white transition-all ease-in-out duration-150'>
             <div className='flex items-center gap-1'>
                 <p className='text-2xl'> {icon}</p>
 
