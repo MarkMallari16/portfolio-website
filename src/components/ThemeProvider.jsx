0193d9eea3ff06c7cda5e63af7c3b563661d16f1
MarkMallari16 <mallariftw01@gmail.com>
2024-07-17 00:59:38 +0800

    fix some folder structure

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
+import { ThemeContext } from '../context/ThemeContext';
 
-const ThemeContext = createContext(null);
 
 export const useTheme = () => {
     return useContext(ThemeContext);
