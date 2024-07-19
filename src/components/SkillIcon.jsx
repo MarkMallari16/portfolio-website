835241a6e2deb3f909058a2e246e0622019b2de6
MarkMallari16 <mallariftw01@gmail.com>
2024-07-19 17:11:17 +0800

    fix some ui

--- src/components/SkillIcon.jsx
+++ src/components/SkillIcon.jsx
@@ -4,7 +4,7 @@ function SkillIcon({ icon, languageName }) {
 
 
     return (
-        <div className='flex border px-4 py-1 rounded-lg border-gra bg-base-200 hover:bg-secondary hover:border-secondary hover:text-white transition-all ease-in-out duration-150'>
+        <div className='flex border px-4 py-1 rounded-lg border-gra bg-base-200 hover:bg-secondary hover:border-secondary transition-all ease-in-out duration-150'>
             <div className='flex items-center gap-1'>
                 <p className='text-2xl'> {icon}</p>
 
