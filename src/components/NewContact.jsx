d62f99de3f6cc4420013c555f135d0c9265c6787
MarkMallari16 <mallariftw01@gmail.com>
2024-09-02 00:28:35 +0800

    change email address on contact

--- src/components/NewContact.jsx
+++ src/components/NewContact.jsx
@@ -6,7 +6,7 @@ const NewContact = () => {
             <div>
                 <h2 className='text-4xl font-medium'>Contact Me!</h2>
                 <h1 className='mt-8 text-4xl lg:text-7xl font-black hover:text-secondary transition-colors ease-in-out duration-200'>
-                    <a href="mailto:mallariftw01@gmail.com">mallariftw@gmail.com</a>
+                    <a href="mailto:mallariftw01@gmail.com">mark.mallari16@gmail.com</a>
                 </h1>
             </div>
 
