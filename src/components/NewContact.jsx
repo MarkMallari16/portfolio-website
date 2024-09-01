ea360eafbebbb31ce9152f74bf2c76501fa8c69b
MarkMallari16 <mallariftw01@gmail.com>
2024-09-01 14:47:57 +0800

    change font in contact form

--- src/components/NewContact.jsx
+++ src/components/NewContact.jsx
@@ -5,7 +5,7 @@ const NewContact = () => {
         <div className='py-20 pb-40 mx-auto lg:max-w-7xl px-6 lg:px-0'>
             <div>
                 <h2 className='text-4xl font-medium'>Contact Me!</h2>
-                <h1 className='mt-8 text-8xl font-black hover:text-secondary transition-colors ease-in-out duration-200'>
+                <h1 className='mt-8 text-4xl lg:text-7xl font-black hover:text-secondary transition-colors ease-in-out duration-200'>
                     <a href="mailto:mallariftw01@gmail.com">mallariftw@gmail.com</a>
                 </h1>
             </div>
