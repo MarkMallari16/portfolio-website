7b1d9308c558a34d0ecf47b1de65cfd5b0ebedac
MarkMallari16 <mallariftw01@gmail.com>
2024-09-02 16:59:58 +0800

    contact fixed

--- src/components/NewContact.jsx
+++ src/components/NewContact.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const NewContact = () => {
     return (
-        <div className='py-40'>
+        <div className='pt-20 pb-40 px-10 lg:px-0'>
             <div>
                 <h2 className='text-4xl font-medium'>Contact Me!</h2>
-                <h1 className='mt-8 text-4xl lg:text-7xl font-black hover:text-secondary transition-colors ease-in-out duration-200'>
-                    <a href="mailto:mallariftw01@gmail.com">mark.mallari16@gmail.com</a>
+                <h1 className='mt-8 text-6xl md:text-6xl lg:text-8xl font-black hover:text-secondary transition-colors ease-in-out duration-200'>
+                    <a href="mailto:mallariftw01@gmail.com">mark
+                        mallari16
+                        @gmail.com</a>
                 </h1>
             </div>
         </div>
