e6ce341e5dada081235710779bde2fe02c24c5c7
MarkMallari16 <mallariftw01@gmail.com>
2025-05-06 18:23:28 +0800

    fix code structure

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -18,9 +18,7 @@ function Theme() {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10 lg:size-6 cursor-pointer" onClick={toggleTheme}>
                             <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .152.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
                         </svg>
-
                     </motion.button>
-
                 ) : (
                     <motion.button
                         initial={{ opacity: 0, scale: 0.9 }}
@@ -33,7 +31,6 @@ function Theme() {
                         </svg>
 
                     </motion.button>
-
                 )}
             </AnimatePresence>
         </>
