7f6bd71906c79ec7380619b05e1b68ee465f9868
Mark Christian Mallari <mallariftw01@gmail.com>
2025-06-19 09:57:08 +0800

    upd: fix ui

--- src/components/TextIcon.jsx
+++ src/components/TextIcon.jsx
@@ -4,10 +4,9 @@ function TextIcon({ icon, languageName, className }) {
 
 
     return (
-        <div className={`flex px-4 py-1 rounded-lg  bg-base-200 hover:bg-base-300 hover:scale-110 hover:border-base-300 transition-all ease-in-out duration-200 ` + className}>
+        <div className={`flex px-4 py-1 rounded-lg  bg-base-200 hover:bg-secondary hover:scale-110  hover:border-base-300 transition-all ease-in-out duration-200 ` + className}>
             <div className='flex items-center gap-1'>
                 <p className='text-2xl'> {icon}</p>
-
                 <p className='select-none'>{languageName}</p>
             </div>
         </div>
